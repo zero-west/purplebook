2e41a395128e08de96fb87b3b2700eba1e8ee52b
yskim <zerowest1001@gmail.com>
2020-07-17 17:51:24 +0900

    Initial commit

---
+++ src/main/java/me/zw/purplebook/PurplebookApplication.java
@@ -0,0 +1,13 @@
+package me.zw.purplebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurplebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurplebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zw/purplebook/PurplebookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zw.purplebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurplebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
