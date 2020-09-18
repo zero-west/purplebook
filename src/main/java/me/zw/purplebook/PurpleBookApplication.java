066df9c982f7e3a085f25a69a09b2964165dac3c
zw <zerowest1001@gmail.com>
2020-09-18 14:21:15 +0900

    CamelCase 적용

--- src/main/java/me/zw/purplebook/PurpleBookApplication.java
+++ src/main/java/me/zw/purplebook/PurpleBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PurplebookApplication {
+public class PurpleBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PurplebookApplication.class, args);
+        SpringApplication.run(PurpleBookApplication.class, args);
     }
 
 }
